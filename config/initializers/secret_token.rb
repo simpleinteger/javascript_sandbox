2b27462c60bbd3ca1cc1b794b6882efa4f2a84c2
Tron <tron@tron-X401A1.(none)>
2013-03-27 18:59:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JavascriptSandbox::Application.config.secret_token = '58d4d11429e68b7842524c6250610006273c118e2b0143b709b88bed6de06ae7ae5973ceb68bc73b65c158550f48cf4f9596595226f13a8c1af20b5139b943ee'
